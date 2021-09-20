57f0ac7f9ae58a3656607337c741b9948957e68d
tonganh <tonganh@users.noreply.github.com>
2021-09-20 14:49:19 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.0d81a010db898114703557932d2509c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc06c2bee7aec2b048c5595a4a1f21ce",
+    "url": "https://tonganh.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "1b46a11b65278f2f13fb",
+    "url": "https://tonganh.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "4e07bbd3ab69d6386d47",
+    "url": "https://tonganh.github.io/task-management-frontend/static/js/2.6497980f.chunk.js"
+  },
+  {
+    "revision": "1b46a11b65278f2f13fb",
+    "url": "https://tonganh.github.io/task-management-frontend/static/js/main.1f24c776.chunk.js"
+  },
+  {
+    "revision": "9dc04e8beee7e84480f8",
+    "url": "https://tonganh.github.io/task-management-frontend/static/js/runtime~main.c8c664e9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://tonganh.github.io/task-management-frontend/precache-manifest.0d81a010db898114703557932d2509c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://tonganh.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
